daaa1f2fd9e70c726dd16bea52bcd4aabda15ed7
Yanilda <synac1@users.noreply.github.com>
2025-03-03 17:57:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let fruits = { "Grapes", "Apples", "Pears", "Oranges"};
 let fruit = fruits[0]; // first fruit 
+function logFirstFruit()
+{
+  let randomNumber= Math.floor(Math.random()*fruits.length);
+  console.log(fruits[randomNumber]);
+}
+logFirstFruit();
+
 
