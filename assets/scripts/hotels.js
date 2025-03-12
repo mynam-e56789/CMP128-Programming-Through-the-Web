91f948febaacd6a2fa3a14f286b38b948a1cc2ce
Yanilda <synac1@users.noreply.github.com>
2025-03-12 22:17:58 +0100

    Constructor Function

---
+++ assets/scripts/hotels.js
@@ -0,0 +1,11 @@
+function Hotel (name, rooms, booked,picture){
+    this.name    = name;
+    this.rooms   = rooms;
+    this.booked  = booked;
+    this.picture = picture;
+    this.checkAvailability =
+            function ()
+            {
+                return this.rooms - this.booked;
+            };
+}
